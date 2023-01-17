ee360b639a2a9edc66d48bc47ff98e8b08fd3b5f
congtoan837 <congtoan837@gmail.com>
2023-01-17 10:32:28 +0700

    #heroku deploy

--- src/main/java/com/poly/ex/ModelMapperConfig.java
+++ src/main/java/com/poly/ex/ModelMapperConfig.java
@@ -3,9 +3,13 @@ package com.poly.ex;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Page;
 
+@Configuration
 public class ModelMapperConfig extends ModelMapper {
+    @Bean
     public ModelMapper modelMapper() {
         // Tạo object và cấu hình
         ModelMapper modelMapper = new ModelMapper();
